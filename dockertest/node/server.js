62437fe5e1e1fa13f3f46c0f1b988ff9d0b73f58
gyeongmin <kgmksw@naver.com>
2025-01-04 20:46:17 +0900

    추가

--- dockertest/node/server.js
+++ dockertest/node/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8080;
 
-app.listen(port, () => {
-  console.log(`서버실행중 ${port} 포트에서 실행중 http://localhost:8080`);
+app.listen(8080, () => {
+  console.log('서버 실행중 http://localhost:8080');
 });
 
 app.get('/', (req, res) => {
-  res.send('<h1>ㅎㅇ</h1>');
-});
+  res.send('안녕dd');
+}); 
+
